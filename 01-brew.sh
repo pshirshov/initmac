0e2ddf4d649db6d7117f9e926d8c6875b9525432
Pavel Shirshov <pshirshov@gmail.com>
2019-06-28 01:02:43 +0100

    oh-my-zsh installer

--- 01-brew.sh
+++ 01-brew.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
-git clone https://github.com/robbyrussell/oh-my-zsh .oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
