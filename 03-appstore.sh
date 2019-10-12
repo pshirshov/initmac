1142bb0267570d593dff53cd24a1d623ac5479cb
Pavel Shirshov <pshirshov@gmail.com>
2019-10-12 21:07:40 +0100

    more appstore apps

--- 03-appstore.sh
+++ 03-appstore.sh
@@ -12,5 +12,15 @@ mas install 1438243180
 mas install 1295203466
 # Automounter
 mas install 1160435653
+# EasyRes
+mas install 688211836
+# AdGuard/Safari
+mas install 1440147259
+# Microsoft TODO
+mas install 1274495053
+# JIRA Cloud
+mas install 1475897096
+# Bitwarden
+mas install 1352778147
 
 echo "SUCCESS"
